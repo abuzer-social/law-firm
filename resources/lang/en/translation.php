cef2500eb9d9f4c082e30341b4ac10e309f7d2fa
zain-here777 <zainii.cs@gmail.com>
2025-06-27 18:44:49 +0500

    latest

---
+++ resources/lang/en/translation.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'header' => [
+        'title' => 'Translation Services',
+        'subtitle' => 'Professional Translation to Support Your Business Operations',
+        'description' => 'Defaa provides subscribers with access to professional translation services through partnerships with specialized, certified Saudi translation firms. These services are overseen by licensed translators, linguistic experts, and consultants, aiming to deliver smart translation solutions that save time and money. Enjoy a professional translation service with speed, accuracy, confidentiality, commitment to deadlines, diverse languages, competitive pricing and specialized expertise to meet all your needs',
+    ],
+];
--- resources/views/pages/translation/translation.blade.php
+++ resources/views/pages/translation/translation.blade.php
@@ -5,29 +5,27 @@
         <header class="translation-header" @if(app()->isLocale('ar')) dir="rtl" @endif>
             <div>
                 <div class="text-center">
-                    <h2 class="primary pb-3 fw-bold">Translation Services</h2>
-                    <h5 class="fw-semibold">Professional Translation to Support Your Business Operations</h5>
+                    <h2 class="primary pb-3 fw-bold">
+                        {{ __('translation.header.title') }}
+                    </h2>
+                    <h5 class="fw-semibold">
+                        {{ __('translation.header.subtitle') }}
+                    </h5>
                 </div>
                 <div class="container">
                     <div class="row align-items-center py-5">
                         <div class="col-lg-6">
-                            <div>
+                            <div @if(app()->isLocale('ar')) style="text-align: right;" @endif>
                                 <h5 class="header-desc pb-4">
-                                    Defaa provides subscribers with access to professional translation services through
-                                    partnerships with specialized, certified Saudi translation firms. These services are
-                                    overseen by licensed translators, linguistic experts, and consultants, aiming to
-                                    deliver smart translation solutions that save time and money.
-                                    Enjoy a professional translation service with speed, accuracy, confidentiality,
-                                    commitment to deadlines, diverse languages, competitive pricing and specialized
-                                    expertise to meet all your needs
+                                    {{ __('translation.header.description') }}
                                 </h5>
                             </div>
                         </div>
                         <div class="col-lg-6">
                             <div>
                                 <img class="img-fluid rounded-5"
-                                     src="{{asset('assets/images/translation-header.png')}}"
-                                     alt="{{ __('business.header.title') }}">
+                                     src="{{ asset('assets/images/translation-header.png') }}"
+                                     alt="{{ __('translation.header.title') }}">
                             </div>
                         </div>
                     </div>
